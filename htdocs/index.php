d42f5c82ee398433553b540af4b068780147499c
jkekoamartin <jmkekoa3@gmail.com>
2017-09-25 22:46:17 -0400

    This is first commit of working development environment

---
+++ htdocs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+
+<div style="color:#ffffff;">
+</div>
+
+<div><div style="color:#FF6600;">
+    <HTML>
+    <HEAD>
+        <TITLE>About Us</TITLE>
+    </HEAD>
+    <div style="text-align: center;"><img src = "img/team.png" style="width:250px;height:250px;"></div>
+    <BODY>
+    <div><HR data-brackets-id='2149'>
+        <H1 data-brackets-id='2150'><span data-brackets-id='2151'
+                                          style="color: #FFFFFF; ">About Us</span></H1>
+        <H3 data-brackets-id='2152'><span data-brackets-id='2153' style="color:#FFFFFF; ">debuggers</span></H3>
+        <HR data-brackets-id='2154'>
+    </div>
+    <div style="color:#ffffff;">
+
+    </HTML>
+
+    <BODY BGCOLOR="00304C">
