39bc6ed58c80d577f75c4632a2ea46512fa63845
jkekoamartin <jmkekoa3@gmail.com>
2017-09-26 00:09:55 -0400

    Made About Page -- really ugly

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,24 +1,53 @@
-<!DOCTYPE html>
 
+<html>
+<head>
+    <title>About Us</title>
+</head>
 
-<div style="color:#ffffff;">
-</div>
+<header>
 
-<div><div style="color:#FF6600;">
-    <html>
-    <head>
-        <title>About Us</title>
-    </head>
     <div style="text-align: center;"><img src = "img/team.png" style="width:250px;height:250px;"></div>
-    <BODY>
-    <div><HR data-brackets-id='2149'>
-        <H1 data-brackets-id='2150'><span data-brackets-id='2151'
-                                          style="color: #FFFFFF; ">About Us</span></H1>
-        <H3 data-brackets-id='2152'><span data-brackets-id='2153' style="color:#FFFFFF; ">debuggers</span></H3>
-        <HR data-brackets-id='2154'>
-    </div>
-    <div style="color:#ffffff;">
 
-    </html>
+    <div id="logo">Page Navigation</div>
 
-    <BODY BGCOLOR="00304C">
+    <nav>
+        <ul>
+            <li><a href="test.php">Home</a>
+            <li><a href="test.php">Dashboard</a>
+            <li><a href="test.php">Account</a>
+        </ul>
+    </nav>
+
+    <div style="text-align: center;"><h2>About Us</h2>
+
+</header>
+<main role="main">
+    <article>
+        <h1>Meet the team:</h1>
+        <ul>
+            <li>Xinyi Jiang</li>
+            <li>James Martin</li>
+            <li>Catherine Xiao</li>
+            <li>Tony Kang</li>
+            <li>Quentin Kauchick</li>
+            <li>Owen Handel</li>
+        </ul>
+    </article>
+    <article>
+        <h1>What we do:</h1>
+        <p>As the team behind Property Smart, we strive to bring meaningful insight to your property-based business. Our software gives you a clear view of the capital expenditures of your property, and more importantly, meaningful feedback about how to plan the future. Whether you just want to improve maintainence or build additions, you can know exactly how much it is going to cost, which helps you avoid the hassle of unforseen costs. Get smart about your property -- Get Property Smart.&nbsp;</p>
+    </article>
+</main>
+<section>
+    <address>
+        <a href="mailto:james.martin@emory.edu">Contact us</a>
+    </address>
+</section>
+
+<footer>
+    <p>&copy; 2017 Property Smart</p>
+
+</footer>
+</html>
+
+<body BGCOLOR="7ECE83">
