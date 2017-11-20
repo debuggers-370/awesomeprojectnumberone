a0e86da9d20fee4b69257c7f419e2bee6a5678a6
jkekoamartin <jmkekoa3@gmail.com>
2017-11-19 21:10:01 -0500

    Merge branch 'sprint7' of https://github.com/debuggers-370/awesomeprojectnumberone into newfeat
    # Conflicts:
    #	resources/views/home.blade.php

--- resources/views/manageunits.blade.php
+++ resources/views/manageunits.blade.php
@@ -9,11 +9,18 @@
                 <div class="w3-container w3-center">
                     <h1>Basic Unit Information</h1>
                 </div>
-                <span class="w3-tag w3-small w3-theme-d5">Unit Name: {{$unit->name}}</span>
+
+                <span class="w3-tag w3-small w3-theme-d5">Unit Name/Number: {{$building->name}}</span>
                 <br>
-                <span class="w3-tag w3-small w3-theme-d5">Unit Renter: {{$unit->renter}}</span>
+                <span class="w3-tag w3-small w3-theme-d5">Tenant: {{$building->address}}</span>
                 <br>
-                <span class="w3-tag w3-small w3-theme-d5">Maintenance Requests: {{$unit->maintenance}}</span>
+                <span class="w3-tag w3-small w3-theme-d5">Property Owner: {{$building->owner}}</span>
+                <br>
+                {{--
+                the below code is trash. need to remove
+               --}}
+
+
             </div>
         </div>
     </div>
