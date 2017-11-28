0ef89a12512c8ab8b7c965bc89275fafdb6be8a8
Elizabeth Jiang <xinyi.jiang2@emory.edu>
2017-11-27 20:32:13 -0500

    Merge branch 'sprint7' of https://github.com/debuggers-370/awesomeprojectnumberone into sprint23
    # Conflicts:
    #	awesomeprojectnumberone.iml

--- resources/views/homeUser.blade.php
+++ resources/views/homeUser.blade.php
@@ -219,9 +219,6 @@
 
 
 
-                            <div class="w3-card-2 w3-round w3-white w3-padding-32 w3-center" style="width:150%;">
-                                <p><i class="fa fa-bug w3-xxlarge"></i></p>
-                            </div>
 
                             <!-- End Right Column -->
                         </div>
