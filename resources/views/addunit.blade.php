a89f240ed7746c0b59245ddeece1b1d4a783aa07
Owen <ohandel@bellsouth.net>
2017-11-28 19:55:33 -0500

    init

--- resources/views/addunit.blade.php
+++ resources/views/addunit.blade.php
@@ -9,7 +9,7 @@
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST"
-                              action="{{ route('managebuilding',$building->id) }}">
+                              action="{{ route('updateUnit',$building->id) }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
